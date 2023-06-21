e4e798851c87b8c43b97d97de8ccafedf087f6dd
iampreetiranjan <46985922+iampreetiranjan@users.noreply.github.com>
2023-06-22 02:13:03 +0530

    Update roberta_training.py
    updated function get_wemb_n

--- roberta_training.py
+++ roberta_training.py
@@ -245,7 +245,9 @@ def get_wemb_n(i_nlu, l_n, hS, num_hidden_layers, all_encoder_layer, num_out_lay
             ed = (i_noln + 1) * hS
             #print(type(i_layer),type(b), type(i_nlu1[0]),type(i_nlu1[1]) ,i_layer,b, i_nlu1[0],i_nlu1[1])
             #wemb_n[b, 0:(i_nlu1[1] - i_nlu1[0]), st:ed] = all_encoder_layer[i_layer][b, i_nlu1[0]:i_nlu1[1], :]
-            wemb_n[b, 0:(i_nlu1[1] - i_nlu1[0]), st:ed] = all_encoder_layer[i_layer][i_nlu1[0]:i_nlu1[1]]
+            #wemb_n[b, 0:(i_nlu1[1] - i_nlu1[0]), st:ed] = all_encoder_layer[i_layer][i_nlu1[0]:i_nlu1[1]]
+            all_encoder_slice = all_encoder_layer[i_layer][i_nlu1[0]:i_nlu1[1]].unsqueeze(0)
+            wemb_n[b, 0:(i_nlu1[1] - i_nlu1[0]), st:ed] = all_encoder_slice
     return wemb_n
 
 def get_wemb_h(i_headers, l_hpu, l_hs, hS, num_hidden_layers, all_encoder_layer, num_out_layers_h):
