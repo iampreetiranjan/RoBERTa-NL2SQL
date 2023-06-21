a711fee7056a4c8b1e11de65a8bdbc28960ff0ac
iampreetiranjan <46985922+iampreetiranjan@users.noreply.github.com>
2023-06-22 01:54:42 +0530

    Update roberta_training.py

--- roberta_training.py
+++ roberta_training.py
@@ -243,7 +243,7 @@ def get_wemb_n(i_nlu, l_n, hS, num_hidden_layers, all_encoder_layer, num_out_lay
             i_layer = num_hidden_layers - 1 - i_noln
             st = i_noln * hS
             ed = (i_noln + 1) * hS
-            print(i_layer,b, i_nlu1[0],i_nlu1[1])
+            print(type(i_layer),type(b), type(i_nlu1[0]),type(i_nlu1[1]) ,i_layer,b, i_nlu1[0],i_nlu1[1])
             wemb_n[b, 0:(i_nlu1[1] - i_nlu1[0]), st:ed] = all_encoder_layer[i_layer][b, i_nlu1[0]:i_nlu1[1], :]
     return wemb_n
 
